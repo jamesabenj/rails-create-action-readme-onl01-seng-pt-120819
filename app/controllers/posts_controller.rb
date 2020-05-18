70bc180ae2e64d62749e00eb62165b22fd7355c4
James Benjamin <james.abenj@gmail.com>
2020-05-18 17:17:33 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,9 +13,9 @@ class PostsController < ApplicationController
 
   def create
     post = Post.new
-  post.title = params[:title]
-  post.description = params[:description]
-  post.save
+    post.title = params[:title]
+    post.description = params[:description]
+    post.save
   end
 
   # add create method here
